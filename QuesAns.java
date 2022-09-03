1cdf62ec3f561476f2d2793a7e089c5e15f29a02
dhana288 <111521363+dhana288@users.noreply.github.com>
2022-09-03 21:29:48 +0530

    Create QuesAns.java

---
+++ QuesAns.java
@@ -0,0 +1,23 @@
+package com.example.quiz_app;
+
+public class QuesAns {
+    public static String question[] ={
+            "Who developed Python Programming Language?",
+            "Which of the following is the correct extension of the Python file?",
+            "Which of the following is used to define a block of code in Python language?",
+            "Which of the following character is used to give single-line comments in Python?",
+            "Which keyword is used for function in Python language?"
+    };
+    public static String choices[][]={
+            {"Guido Van Rossum","Dennis Ritchie","James Gosling","Wick Van Rossum"},
+            {".python",".p",".py","None of these"},
+            {"Key","Brackets","Indentation","All of the above"},
+            {"//","#","!","/*"}
+    };
+    public static String correctans[]={
+            "Guido Van Rossum",
+            ".py",
+            "Indentation",
+            "#"
+    };
+}
